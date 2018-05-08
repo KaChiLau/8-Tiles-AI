ea9b1d63ba6e9e41bad17151c1b434f7e83b1337
arthemg <arthemg@gmail.com>
2018-05-08 15:21:09 -0700

    Deleted Unnecessary files

--- EightTile.py
+++ EightTile.py
@@ -5,10 +5,11 @@ import util
 _goal_state = [[1, 2, 3],
                [4, 5, 6],
                [7, 8, 0]]
-			   
+
 _init_state = [[1, 2, 3],
-			   [7, 4, 6],
-			   [0, 5, 8]]
+               [7, 4, 6],
+               [0, 5, 8]]
+
 
 def index(item, seq):
     # helper function that returns -1 for non-found index value of a seq
@@ -16,6 +17,7 @@ def index(item, seq):
         return seq.index(item)
     else:
         return -1
+
 
 class EightPuzzle:
 
@@ -75,97 +77,96 @@ class EightPuzzle:
             return self._parent._generate_solution_path(path)
 
     def solve(self, heur, algorithm):
-		# performs BFS, UCS, or A* search for goal state.
+        # performs BFS, UCS, or A* search for goal state.
         # heur(puzzle) - heuristic function, returns an integer
-
-		def is_solved(puzzle):
-			return puzzle.adj_matrix == _goal_state
-		
-		fringe = [self]  # list that contains all nodes to be expanded/examined
-		path = []  # list that contains the final path taken through the tree to the goal state
-		move_count = 0
-		while len(fringe) > 0:
-			x = fringe.pop(0)
-			move_count += 1
-			if (is_solved(x)):
-				if len(path) > 0:
-					return x._generate_solution_path([]), move_count
-				else:
-					return [x]
-
-			successor = x._generate_moves()
-			fringe_index = path_index = -1
-			for move in successor:
-                # check for if the node has already been seen
-				fringe_index = index(move, fringe)
-				path_index = index(move, path)
-				heurval = heur(move)
-				# total utility is defined as heuristic value of the matrix after move as well as its depth in the tree- 
-				# the lower the depth, the more costly it is to expand that node, and the costlier it is. For all intents
-				# and purposes, the depth of the node in the tree is taken as the 'cost' of that node.
-				total_utility = heurval + move._depth 
-
-				if path_index == -1 and fringe_index == -1:
-					move._heurval = heurval
-					fringe.append(move)
-				elif fringe_index > -1:
-					copy = fringe[fringe_index]
-					if total_utility < copy._heurval + copy._depth:
-                        # copy the successor's values over the existing node
-						copy._heurval = heurval
-						copy._parent = move._parent
-						copy._depth = move._depth
-				elif path_index > -1:
-					copy = path[path_index]
-					if total_utility < copy._heurval + copy._depth:
-						move._heurval = heurval
-						path.remove(copy)
-						fringe.append(move)
-
-			path.append(x)
-			if algorithm == 'BFS' or algorithm == 'UCS':
-				fringe = sorted(fringe, key=lambda p: p._depth)
-			elif algorithm == 'A*':
-				fringe = sorted(fringe, key=lambda p: p._heurval + p._depth) 
-				
-        # if the goal state is never reached, return an empty list (representing the path taken is null) 
-		# and 0 for the number of steps.
-		return [], 0
-	
-    def solve_DFS(self):
-        # Performs DFS search for goal state.
-        # heur(puzzle) - heuristic function, returns an integer
-		
 
         def is_solved(puzzle):
             return puzzle.adj_matrix == _goal_state
 
-        #fringe = [self]  # list that contains all nodes to be expanded/examined
-        #path = []  # list that contains the final path taken through the tree to the goal state
+        fringe = [self]  # list that contains all nodes to be expanded/examined
+        path = []  # list that contains the final path taken through the tree to the goal state
+        move_count = 0
+        while len(fringe) > 0:
+            x = fringe.pop(0)
+            move_count += 1
+            if (is_solved(x)):
+                if len(path) > 0:
+                    return x._generate_solution_path([]), move_count
+                else:
+                    return [x]
+
+            successor = x._generate_moves()
+            fringe_index = path_index = -1
+            for move in successor:
+                # check for if the node has already been seen
+                fringe_index = index(move, fringe)
+                path_index = index(move, path)
+                heurval = heur(move)
+                # total utility is defined as heuristic value of the matrix after move as well as its depth in the tree-
+                # the lower the depth, the more costly it is to expand that node, and the costlier it is. For all intents
+                # and purposes, the depth of the node in the tree is taken as the 'cost' of that node.
+                total_utility = heurval + move._depth
+
+                if path_index == -1 and fringe_index == -1:
+                    move._heurval = heurval
+                    fringe.append(move)
+                elif fringe_index > -1:
+                    copy = fringe[fringe_index]
+                    if total_utility < copy._heurval + copy._depth:
+                        # copy the successor's values over the existing node
+                        copy._heurval = heurval
+                        copy._parent = move._parent
+                        copy._depth = move._depth
+                elif path_index > -1:
+                    copy = path[path_index]
+                    if total_utility < copy._heurval + copy._depth:
+                        move._heurval = heurval
+                        path.remove(copy)
+                        fringe.append(move)
+
+            path.append(x)
+            if algorithm == 'BFS' or algorithm == 'UCS':
+                fringe = sorted(fringe, key=lambda p: p._depth)
+            elif algorithm == 'A*':
+                fringe = sorted(fringe, key=lambda p: p._heurval + p._depth)
+
+                # if the goal state is never reached, return an empty list (representing the path taken is null)
+        # and 0 for the number of steps.
+        return [], 0
+
+    def solve_DFS(self):
+        # Performs DFS search for goal state.
+        # heur(puzzle) - heuristic function, returns an integer
+
+        def is_solved(puzzle):
+            return puzzle.adj_matrix == _goal_state
+
+        # fringe = [self]  # list that contains all nodes to be expanded/examined
+        # path = []  # list that contains the final path taken through the tree to the goal state
 
         fringe = util.Stack()
         path = list()
         visited = list()
         move_count = 0
-        
+
         fringe.push(self)
         while not fringe.isEmpty():
             node = fringe.pop()
             move_count += 1
 
-            if(node not in visited):
+            if (node not in visited):
                 visited.append(node)
-            
+
                 if (is_solved(node)):
                     return node._generate_solution_path([]), move_count
-                    
+
                 successor = node._generate_moves()
                 for move in successor:
                     move.parent = node
                     fringe.push(move)
-             
+
         return [], 0
-	
+
 
 def heur(puzzle, item_total_calc, total_calc):
     """
@@ -191,8 +192,8 @@ def heur(puzzle, item_total_calc, total_calc):
                 target_row = 2
             t += item_total_calc(row, target_row, col, target_col)
 
-
     return total_calc(t)
+
 
 # both h_manhattan and h_default are to be used with the heur() function above
 # the lambda arguments in the return statement below specify how to use total_calc and item_total_calc above.
@@ -208,56 +209,57 @@ def h_default(puzzle):
 
 
 def main():
-	utility = util.utility()
-	p = EightPuzzle()
-	shuffle = utility.shuffle(p, 20)
-	print "Starting puzzle:"
-	print shuffle
-	#print p
-	
-	selection = raw_input("Which algorithm would you like to use to solve this matrix? (DFS, BFS, UCS, A*, or All for all four) \n")
-	if selection == "DFS":
-		path, count = shuffle.solve_DFS()
-		path.reverse()
-		print "Path to goal state:"
-		for i in path:
-			print i
-		print "Solved with DFS search in", count, "node expansions"
-	elif selection == "BFS":
-		path, count = shuffle.solve(h_default, 'BFS')
-		path.reverse()
-		print "Path to goal state:"
-		for i in path:
-			print i
-		print "Solved with BFS search in", count, "node expansions"
-	elif selection == "UCS":
-		path, count = shuffle.solve(h_default, 'UCS')
-		path.reverse()
-		print "Path to goal state:"
-		for i in path:
-			print i
-		print "Solved with UCS search in", count, "node expansions"
-	elif selection == "A*":
-		path, count = shuffle.solve(h_manhattan, 'A*')
-		path.reverse()
-		print "Path to goal state:"
-		for i in path:
-			print i
-		print "Solved with A* search utilizing Manhattan distance hueuristic in", count, "node expansions"
-	elif selection == "All" or "all":
-		path, count = shuffle.solve(h_manhattan, 'A*')
-		path.reverse()
-		print "Path to goal state:"
-		for i in path:
-			print i
-		print "Solved with A* search utilizing Manhattan distance hueuristic in", count, "node expansions"
-		path, count = shuffle.solve(h_default, 'UCS')
-		print "Solved with UCS search in", count, "node expansions"
-		path, count = shuffle.solve(h_default, 'BFS')
-		print "Solved with BFS search in", count, "node expansions"
-		path, count = shuffle.solve_DFS()
-		print "Solved with DFS search in", count, "node expansions"
-	
+    utility = util.utility()
+    p = EightPuzzle()
+    shuffle = utility.shuffle(p, 20)
+    print "Starting puzzle:"
+    print shuffle
+    # print p
+
+    selection = raw_input(
+        "Which algorithm would you like to use to solve this matrix? (DFS, BFS, UCS, A*, or All for all four) \n")
+    if selection == "DFS":
+        path, count = shuffle.solve_DFS()
+        path.reverse()
+        print "Path to goal state:"
+        for i in path:
+            print i
+        print "Solved with DFS search in", count, "node expansions"
+    elif selection == "BFS":
+        path, count = shuffle.solve(h_default, 'BFS')
+        path.reverse()
+        print "Path to goal state:"
+        for i in path:
+            print i
+        print "Solved with BFS search in", count, "node expansions"
+    elif selection == "UCS":
+        path, count = shuffle.solve(h_default, 'UCS')
+        path.reverse()
+        print "Path to goal state:"
+        for i in path:
+            print i
+        print "Solved with UCS search in", count, "node expansions"
+    elif selection == "A*":
+        path, count = shuffle.solve(h_manhattan, 'A*')
+        path.reverse()
+        print "Path to goal state:"
+        for i in path:
+            print i
+        print "Solved with A* search utilizing Manhattan distance hueuristic in", count, "node expansions"
+    elif selection == "All" or "all":
+        path, count = shuffle.solve(h_manhattan, 'A*')
+        path.reverse()
+        print "Path to goal state:"
+        for i in path:
+            print i
+        print "Solved with A* search utilizing Manhattan distance hueuristic in", count, "node expansions"
+        path, count = shuffle.solve(h_default, 'UCS')
+        print "Solved with UCS search in", count, "node expansions"
+        path, count = shuffle.solve(h_default, 'BFS')
+        print "Solved with BFS search in", count, "node expansions"
+        path, count = shuffle.solve_DFS()
+        print "Solved with DFS search in", count, "node expansions"
+
 
 if __name__ == "__main__":
     main()
